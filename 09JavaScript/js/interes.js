43d88ffa06cb7750f69908a3d5f83d7a4964c346
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2022-03-11 17:52:38 -0600

    ejercicios

--- 09JavaScript/js/interes.js
+++ 09JavaScript/js/interes.js
@@ -2,10 +2,21 @@
 Vamos a hacer una funcion que se encarge de validar la entrada de numeros
 por medio de una expresion regular
 */
-function validar(e){
+function validarn(e){
     var teclado=(document.all)?e.keyCode: e.which;
     if(teclado==8) return true;
     var patron=/[0-9\d.]/;
     var codigo=String.fromCharCode(teclado);
     return patron.test(codigo);
+}
+function interes(){
+    var valor=document.formulario.cantidad.value;
+    var resul=parseInt(valor);
+    var interes=resul*0.037;
+    var total=interes+resul;
+    document.formulario.sueldoI.value="$"+total;
+}
+function borrard(){
+    document.formulario.cantidad.value="";
+    document.formulario.sueldoI.value="";
 }
